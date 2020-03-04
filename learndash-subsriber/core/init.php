a007445a2e61532b27d51740e67eb4871ecebc8e
stevegiorgi <steve.giorgi@ymail.com>
2020-03-04 04:57:19 -0800

    Testing w/get_post_type

--- learndash-subsriber/core/init.php
+++ learndash-subsriber/core/init.php
@@ -30,7 +30,7 @@ function ldsubscriber_board_shortcode(){
 
 $modules = array(172);
 
-if (is_single(172)) {
+if (is_single($modules) && 'sfwd-lessons' == get_post_type()) {
   $board = new Helpers\BoardPage();
   return $board->renderform();
 }
